0345a6c4661004f84abe12a3bcfdc797ea517bd2
Christian Danscheid <christian.danscheid@outlook.de>
2019-05-29 11:39:11 +0200

    Calibration done

--- src/main/main.py
+++ src/main/main.py
@@ -17,27 +17,31 @@ def main():
     pwm.set_pwm(5, 0, 600)
 
     # TODO: Calibrate servos
-    # servo1 = LogicalServo(PhysicalServo(pwm, 0, 150, 600))
-    # servo2 = LogicalServo(PhysicalServo(pwm, 1, 150, 600))
-    # servo3 = LogicalServo(PhysicalServo(pwm, 2, 150, 600))
-    # servo4 = LogicalServo(PhysicalServo(pwm, 3, 150, 600))
-    # servo5 = LogicalServo(PhysicalServo(pwm, 4, 150, 600))
-    # servo6 = LogicalServo(PhysicalServo(pwm, 5, 150, 600))
+    armRotatation1 = LogicalServo(PhysicalServo(pwm, 5, 400, 2050))
+    armRotation2 = LogicalServo(PhysicalServo(pwm, 4, 400, 2050))
+    armRotation3 = LogicalServo(PhysicalServo(pwm, 3, 400, 2050))
+    armRotation4 = LogicalServo(PhysicalServo(pwm, 2, 400, 2050))
+    clawRotation = LogicalServo(PhysicalServo(pwm, 1, 350, 1800))
+    claw = LogicalServo(PhysicalServo(pwm, 0, 350, 1800))
 
-    # arm = RoboArm(servo1, servo2, servo3, servo4, servo5, servo6)
+    arm = RoboArm(armRotatation1, armRotation2, armRotation3, armRotation4, clawRotation, claw)
 
-    # arm.claw.open()
-    # arm.claw.close()
+    arm.claw.open()
+    arm.claw.close()
 
-    # servo1.setAngle(0)
-    # time.sleep(1)
-    # servo1.setAngle(90)
-    # time.sleep(1)
-    # servo1.setAngle(180)
-    # time.sleep(1)
-    # servo1.setAngle(90)
-    # time.sleep(1)
-    # servo1.setAngle(0)
+    armRotatation1.setAngle(0)
+    time.sleep(1)
+
+    armRotatation1.setAngle(90)
+    time.sleep(1)
+
+    armRotatation1.setAngle(180)
+    time.sleep(1)
+
+    armRotatation1.setAngle(90)
+    time.sleep(1)
+
+    armRotatation1.setAngle(0)
 
     # coords = extramath.cat2sph(10.0, 2.0, 0.0)
     # print("r\t\t= {:f}LE\ninclination\t= {:f}°\nazimuth\t\t= {:f}°".format(coords.radius, math.degrees(coords.inclination), math.degrees(coords.azimuth)))
